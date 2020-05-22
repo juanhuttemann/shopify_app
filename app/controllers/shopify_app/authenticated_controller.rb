141a5b56ac5d82c64974c7016e0b6a2f35d0b85c
Aditya Mattos <aditya.mattos@shopify.com>
2020-05-22 12:45:58 +0530

    Skip CSRF check if a valid JWT is passed in

--- app/controllers/shopify_app/authenticated_controller.rb
+++ app/controllers/shopify_app/authenticated_controller.rb
@@ -3,6 +3,12 @@ module ShopifyApp
   class AuthenticatedController < ActionController::Base
     include ShopifyApp::Authenticated
 
-    protect_from_forgery with: :exception
+    protect_from_forgery with: :exception, unless: :valid_jwt_header?
+
+    private
+
+    def valid_jwt_header?
+      jwt_shopify_domain.present?
+    end
   end
 end
